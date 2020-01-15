8fec20181b93e445dc235b37d91bf19988df9afb
sina <sina.majdieh@gmail.com>
2020-01-15 15:28:23 +0330

    #

--- DigitSeperator.go
+++ DigitSeperator.go
@@ -1,16 +1,20 @@
 package digitsep
 
-import (
-	"strconv"
-)
+
 
 //Seperate digits
-func SepDigits(n int) string {
-	s := strconv.Itoa(n)
-	if len(s) < 4 {
+func SepDigits(s string) string {
+	length := len(s)
+	for i := len(s)-1 ; i >= 0 ; i--{
+		if s[i] == '.'{
+			length = i
+			break
+		}
+	}
+	if length < 4 {
 		return s
 	} else {
-		for i := len(s) - 3; i > 0; i -= 3 {
+		for i := length - 3; i > 0; i -= 3 {
 			s = s[:i] + "," + s[i:]
 		}
 		return s
